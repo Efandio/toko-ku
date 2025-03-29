40c886a57dc2520ca6d188d8b78c77f2595a8f29
Efandio <efandio904@gmail.com>
2025-03-30 02:56:58 +0700

    cart page styling

--- src/components/pages/CartPage.tsx
+++ src/components/pages/CartPage.tsx
@@ -1,14 +1,46 @@
+import { useSelector } from "react-redux";
 import Navbar from "../ui/Navbar";
+import { RootState } from "@/app/store";
+import { Card, CardContent } from "../ui/card";
+import { Button } from "../ui/button";
+import { Trash2 } from "lucide-react";
 
 const CartPage = () => {
 
-    
+    const cart = useSelector((state: RootState) => state.cart.items);
 
     return (
         <main className="">
             <Navbar navTitle={"Keranjang-Ku"} />
-            <main className="pt-24 text-white">
-                
+            <main className="pt-24 grid lg:grid-cols-1 lg:gap-5 lg:px-10">
+                {cart.map((a) => (
+                    <Card>
+                        <CardContent className="flex justify-between">
+                            <div className="flex items-center gap-4">
+                                <div>
+                                    <img className="w-24 h-24" src={a.image} alt={a.title} />
+                                </div>
+                                <div>
+                                    <h1 className="font-medium">{a.title}</h1>
+                                    <h2 className="text-xl">${a.price}</h2>
+                                </div>
+                            </div>
+                            <div className="flex flex-col gap-2 justify-center items-center">
+                                <div>
+                                    <Trash2 className="cursor-pointer" color="#ff0000" strokeWidth={1.5} />
+                                </div>
+                                <div className="gap-2 flex">
+                                    <Button className="text-sm" size='sm'>-</Button>
+                                    <span>{a.quantity}</span>
+                                    <Button className="text-sm" size='sm'>+</Button>
+                                </div>
+                                <div className="flex gap-2">
+                                    <Button size='sm'>Checkout</Button>
+                                </div>
+                            </div>
+                        </CardContent>
+                    </Card>
+                ))}
             </main>
         </main>
     )
--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -5,12 +5,13 @@ interface Navbar {
     navTitle: string;
     input?: ReactNode;
     children?: ReactNode;
+    className?: string;
 }
  
-const Navbar: React.FC<Navbar> = ({ navTitle, input, children }) => {
+const Navbar: React.FC<Navbar> = ({ navTitle, input, children, className }) => {
 
     return (
-        <nav className="text-white flex justify-between items-center py-2 w-full px-20 fixed">
+        <nav className={`text-white flex justify-between items-center py-2 w-full px-20 fixed z-50 ${className}`}>
             <Link to={"/"}>
                 <div className="flex items-center gap-1 font-semibold cursor-pointer">
                 <ShoppingBag size={24} color="#ffffff" strokeWidth={1.5} />    
